b2675356fafccec38b91dbdd46ff4a63af2e0bbf
vitorcontiz <vitorcontizgit@gmail.com>
2022-09-22 23:16:44 -0300

    feature: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import './App.css'
-
 function App() {
 
 
   return (
-    <div className="bg-purple-800">
-      <ul className='text-white font-semibold'>
-        <li>Cadastro O.S.</li>
-        <li>Consulta O.S.</li>
+    <div className="h-12 bg-gradient-to-r from-purple-500 justify-end to-purple-900 flex items-center p-8 rounded-b-lg">
+      <ul className='text-white font-semibold flex text-xl space-x-2 font-sans'>
+        <li className=" cursor-pointer inline-block border border-violet-500 rounded py-2 px-4 bg-violet-500 hover:bg-violet-700 text-white">Cadastro O.S.</li>
+        <li className="cursorinline-block border border-violet-500 rounded py-2 px-4 bg-violet-500 hover:bg-violet-700 text-white">Consulta O.S.</li>
       </ul>
     </div>
   )
