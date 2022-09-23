460be6e128f4fabb189eef9a8d69fdd9862225d4
vitorcontiz <vitorcontizgit@gmail.com>
2022-09-22 21:53:13 -0300

    feature: navbar init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="bg-purple-800">
+      <ul className='text-white font-semibold'>
+        <li>Cadastro O.S.</li>
+        <li>Consulta O.S.</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
