328061bf4a021c1f9893a6b512d7286110605a94
vitorcontiz <vitorcontizgit@gmail.com>
2022-09-25 22:23:48 -0300

    feat combo box

--- src/pages/CadastroOs/CadastroOs.jsx
+++ src/pages/CadastroOs/CadastroOs.jsx
@@ -4,7 +4,7 @@ import FormOs from "./components/FormOs"
 const CadastroOs = () => {
     return (
         <div className="m-4 flex flex-col space-y-3">
-            <h1 className="text-2xl">Cadastro de Ordem de Serviço</h1>
+            <h1 className="text-2xl font-semibold uppercase">Cadastro de Ordem de Serviço</h1>
             <FormOs />
         </div>
     )
--- src/pages/CadastroOs/components/FormOs.jsx
+++ src/pages/CadastroOs/components/FormOs.jsx
@@ -29,8 +29,26 @@ export default function FormOs() {
                     </div>
                 </div>
                 <div className="relative z-0 mb-6 w-1/4 group">
-                    <input type="text" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="cpf" id="cpf" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-500 focus:outline-none focus:ring-0 focus:border-purple-600 peer" placeholder=" " required="" />
+                    <input type="text" pattern="(\d{3}\.?\d{3}\.?\d{3}-?\d{2})|(\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2})" name="cpf" id="cpf" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-500 focus:outline-none focus:ring-0 focus:border-purple-600 peer" placeholder=" " required="" />
                     <label htmlFor="cpf" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-purple-600 peer-focus:dark:text-purple-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">CPF</label>
+                </div>
+                <div className="relative z-0 mb-6 w-1/4 group">
+                    <select type="text" name="procedimentos" id="procedimentos" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-purple-500 focus:outline-none focus:ring-0 focus:border-purple-600 peer" placeholder=" " required="">
+                        {procedimentos.map((procedimento) => {
+                            return <option key={procedimento.id}>
+
+
+                                <button type="button" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    {procedimento.nome}
+                                    <span class="inline-flex justify-center items-center ml-2 w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
+                                        {procedimento.valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+                                    </span>
+                                </button>
+
+                            </option>
+                        })}
+                    </select>
+                    <label htmlFor="procedimentos" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-purple-600 peer-focus:dark:text-purple-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">CPF</label>
                 </div>
                 <button type="submit" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Submit</button>
             </form>
