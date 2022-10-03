1c1440c700f00420c9ce33895ba3878ae2a69efe
vitorcontiz <vitorcontizgit@gmail.com>
2022-10-02 22:23:59 -0300

    refactor: button style

--- src/pages/CadastroOs/components/ProcedureCard.jsx
+++ src/pages/CadastroOs/components/ProcedureCard.jsx
@@ -2,9 +2,9 @@
 
 export default function ProcedureCard({ nome, valor }) {
     return (
-        <div className="rounded-lg flex items-center shadow-purple-400 p-4 w-52 bg-purple-700">
-            <div className="font-semibold uppercase rounded-md bg-cyan-200 p-2">{nome}</div>
-            <div className="  bg-green-800 rounded-md p-2">{valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</div>
+        <div className="cursor-pointer rounded-lg flex items-center shadow-purple-400 p-4 w-52 bg-purple-700">
+            <div className="font-semibold flex items-center  w-48 uppercase rounded-md bg-orange-500 p-2">{nome}</div>
+            <div className="  bg-green-900 font-bold rounded-md p-2">{valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</div>
         </div>
     )
 }
